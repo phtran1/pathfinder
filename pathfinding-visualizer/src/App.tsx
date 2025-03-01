15454219bdc321dd843a8f31faf63ebcade288b1
Phillip Tran <tranphillip206@gmail.com>
2025-02-28 17:08:44 -0800

    Added Contexts

--- pathfinding-visualizer/src/App.tsx
+++ pathfinding-visualizer/src/App.tsx
@@ -1,5 +1,14 @@
+import { PathfindingProvider } from "./context/PathfindingContext";
+import { TileProvider } from "./context/TileContext";
+
 function App() {
-  return (<h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">Hello world!</h1>)
+  return (
+    <PathfindingProvider>
+      <TileProvider>
+        <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">Hello world!</h1>
+      </TileProvider>
+    </PathfindingProvider>
+  );
 }
 
 export default App
